e5a9bf8c5f84678568b361d8cc2d093566bc6e40
Victor Sacramento <victorhsacra@gmail.com>
2025-06-03 20:43:11 -0300

    agooora ta pronto

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const mysql = require("mysql2/promise");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: __dirname + "/preload.js",
+    },
   });
 
   win.loadFile("pages/index.html");
 };
 
-ipcMain.handle("salvar-livro", async function(){
+ipcMain.handle("listar-livros", async function () {
+  const conexao = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "livros_db",
+  });
+  const query = await conexao.execute("select * from livros");
+  await conexao.end();
 
-  var conexao = await mysql.createConnection({
+  return query[0];
+});
+
+ipcMain.handle("salvar-livro", async (event, titulo, autor) => {
+  const conexao = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
     database: "livros_db",
   });
 
-conexao.execute("insert into feedbacks (nome, mensagem) values (?, ?)",)
+  await conexao.execute("INSERT INTO livros (titulo, autor) VALUES (?, ?)", [titulo, autor]);
+  await conexao.end();
 });
-
 
 app.whenReady().then(() => {
   createWindow();
--- preload.js
+++ preload.js
@@ -1,14 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-function salvarLivro(titulo, autor) {
+function salvarLivro( titulo, autor ) {
+    console.log('Salvando livro:', titulo, autor);
     return ipcRenderer.invoke('salvar-livro', titulo, autor);
 }
 
 function listarLivros() {
+    console.log('Listando livros');
+    
     return ipcRenderer.invoke('listar-livros');
 }
 
 contextBridge.exposeInMainWorld('api', {
     salvarLivro,
-    listarLivros
+    listarLivros,
 });
